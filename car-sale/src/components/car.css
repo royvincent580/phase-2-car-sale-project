e14979af33a74291b61a24677e5b7079e82fbbe1
levimutai <levimutai@example.com>
2025-07-25 02:53:57 +0300

    initial commit

---
+++ car-sale/src/components/car.css
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import CarCard from '../components/CarCard';
+import CarDetail from '../components/CarDetail';
+import SearchBar from '../components/SearchBar';
+import '../styles/Cars.css';
